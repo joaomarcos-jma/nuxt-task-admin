7525ccb11d28a29d255ba95081398957c23ec1b9
João Marcos <joaomarcos.src@gmail.com>
2023-06-02 13:31:41 -0300

    :tada: feat: start project

---
+++ components/shared/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand href="#">House Rules</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
